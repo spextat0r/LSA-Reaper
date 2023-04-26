83eb74f2f208d2183b1f12ded17c7c50024e5e8c
Daniel <57763139+samiam1086@users.noreply.github.com>
2023-04-26 11:52:52 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 sudo python3 -m pip install -r requirements.txt
 sudo python3 -m pip install pypykatz --force-reinstall --upgrade
-cd ..
-git clone https://github.com/SecureAuthCorp/impacket.git
-cd impacket
-sudo python3 setup.py install
+if [ -d "../impacket" ] 
+then
+    echo "Impacket is already downloaded" 
+else
+    cd ..
+    git clone https://github.com/SecureAuthCorp/impacket.git
+    cd impacket
+    sudo python3 setup.py install
+fi
+
