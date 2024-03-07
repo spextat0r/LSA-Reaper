be4ac7ab72ec349ebf76e0b18a2a4ace9286a888
Daniel <57763139+samiam1086@users.noreply.github.com>
2024-03-06 20:21:49 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ sudo apt-get update
 sudo apt-get install python3-apt -y
 sudo apt-get install samba -y
 sudo apt-get install mono-complete -y
+sudo apt-get install mingw-w64 -y
 sudo python3 -m pip install -r requirements.txt
 sudo python3 -m pip install pypykatz --force-reinstall --upgrade
 if [ -d "../impacket" ] 
